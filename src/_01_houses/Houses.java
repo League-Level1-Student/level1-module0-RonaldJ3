8f89295f84ed13187e752646ce4e4eb290283f66
RonaldJ3 <rrizzonj@gmail.com>
2020-04-11 11:27:53 -0700

    I continued houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -3,6 +3,22 @@ import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	public void run() {
 	Robot r = new Robot();
+	r.setSpeed(10);
+	r.setPenWidth(10);
 	r.setX(50);
+	r.setRandomPenColor();
+	r.penDown();
+	r.move(100);
+	r.turn(90);
+	r.move(30);
+	r.turn(90);
+	r.move(100);
+	r.move(10);
+	r.setPenColor(0,120,0);
+	r.turn(-90);
+	r.move(20);
+	r.penUp();
+	r.move(100);
 	}
+	void
 }

776f2494415d46bf4f44fecdbe28c147d03ee821
RonaldJ3 <rrizzonj@gmail.com>
2020-05-09 11:25:28 -0700

    I continued houses

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -11,24 +11,24 @@ public class Houses {
 		r.setX(50);
 		r.setRandomPenColor();
 		r.penDown();
-		drawHouse("small", "Red");
+		drawHouse("large", "red");
 	}
 
 	public void drawHouse(String height, String color) {
 		if (color.equals("Red")) {
 			r.setPenColor(200, 0, 0);
-		}
-		else if (color.equals("Green")){
-			r.setPenColor(0,200,0);
-		}
-		else if (color.equals("blue")){
-			r.setPenColor(0,0,200);
+		} else if (color.equals("Green")) {
+			r.setPenColor(0, 200, 0);
+		} else if (color.equals("blue")) {
+			r.setPenColor(0, 0, 200);
 		}
 		if (height.equals("small")) {
 			r.move(60);
+			r.turn(45);
+			r.move(15);
 			r.turn(90);
-			r.move(30);
-			r.turn(90);
+			r.move(15);
+			r.turn(45);
 			r.move(60);
 			r.setPenColor(0, 120, 0);
 			r.turn(-90);
@@ -38,20 +38,19 @@ public class Houses {
 		}
 		if (color.equals("Red")) {
 			r.setPenColor(200, 0, 0);
-		}
-		else if (color.equals("Green")){
-			r.setPenColor(0,200,0);
-		}
-		else if (color.equals("blue")){
-			r.setPenColor(0,0,200);
+		} else if (color.equals("Green")) {
+			r.setPenColor(0, 200, 0);
+		} else if (color.equals("blue")) {
+			r.setPenColor(0, 0, 200);
 		}
 		if (height.equals("medium")) {
 			r.move(120);
-			r.turn(90);
+			r.turn(45);
 			r.move(30);
 			r.turn(90);
-			r.move(60);
-			r.move(60);
+			r.move(30);
+			r.turn(45);
+			r.move(120);
 			r.setPenColor(0, 120, 0);
 			r.turn(-90);
 			r.move(20);
@@ -61,20 +60,19 @@ public class Houses {
 		}
 		if (color.equals("Red")) {
 			r.setPenColor(200, 0, 0);
-		}
-		else if (color.equals("Green")){
-			r.setPenColor(0,200,0);
-		}
-		else if (color.equals("blue")){
-			r.setPenColor(0,0,200);
+		} else if (color.equals("Green")) {
+			r.setPenColor(0, 200, 0);
+		} else if (color.equals("blue")) {
+			r.setPenColor(0, 0, 200);
 		}
 		if (height.equals("large")) {
 			r.move(250);
-			r.turn(90);
+			r.turn(45);
 			r.move(30);
 			r.turn(90);
-			r.move(60);
-			r.move(190);
+			r.move(30);
+			r.turn(45);
+			r.move(250);
 			r.setPenColor(0, 120, 0);
 			r.turn(-90);
 			r.move(20);

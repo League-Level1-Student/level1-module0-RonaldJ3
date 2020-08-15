b7c1109318354dfbe2236cc87d41e275cf61fc51
RonaldJ3 <rrizzonj@gmail.com>
2020-08-15 11:23:11 -0700

    I finishd fortune cookie and started jack I the box

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,16 +1,19 @@
 package _07_fortune_cookie;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class FortuneCookie implements ActionListener {
 	JFrame frame = new JFrame();
-	JButton button = new JButton();
+	JButton button = new JButton("click me");
 
 	public FortuneCookie() {
 		frame.add(button);
+		frame.setLocationRelativeTo(null);
 		frame.pack();
 		button.addActionListener(this);
 	}
@@ -23,6 +26,8 @@ public class FortuneCookie implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		int rand = new Random().nextInt(5);
+		JOptionPane.showMessageDialog(null,rand);
 		
 	}
 }

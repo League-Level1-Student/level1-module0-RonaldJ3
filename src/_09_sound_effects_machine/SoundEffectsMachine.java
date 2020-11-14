15d817b3b628eac8313c06321d3488d57e1d563f
Ronald Jenkins <rrizonj@Gmail.com>
2020-11-14 11:31:34 -0800

    I contuied sound effets machine

--- src/_09_sound_effects_machine/SoundEffectsMachine.java
+++ src/_09_sound_effects_machine/SoundEffectsMachine.java
@@ -6,17 +6,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class SoundEffectsMachine implements ActionListener {
+    JButton button1;
+    JButton button2;
+    JButton button3;
     public void showButton() {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        JButton button = new JButton();
+        button1 = new JButton("click me 1");
+        button2 = new JButton("click me 2");
+        button3 = new JButton("click me 3");
         frame.add(panel);
-        panel.add(button);
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
         frame.pack();
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
-        button.addActionListener(this);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
     }
+
 
     private void playSound(String fileName) {
         AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
@@ -25,6 +35,16 @@ public class SoundEffectsMachine implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        playSound("sawing-wood-daniel_simon.wav");
+        if (e.getSource()==button1) {
+            playSound("sawing-wood-daniel_simon.wav");
+        }
+        if (e.getSource()==button2) {
+            playSound("boing.wav");
+        }
+        if (e.getSource()==button3) {
+            playSound("beach-noises.wav");
+        }
     }
 }
+
+

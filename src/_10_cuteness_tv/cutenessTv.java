6bb5a927e6db5ef09c00e82c8377cfb8b6a68a2d
Ronald Jenkins <rrizonj@Gmail.com>
2021-01-23 10:21:29 -0800

    workeing on cuteness tv

---
+++ src/_10_cuteness_tv/cutenessTv.java
@@ -0,0 +1,41 @@
+package _10_cuteness_tv;
+
+import java.net.URI;
+
+public class cutenessTv {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    void showDucks() {
+    playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+}
+
+    void showFrog() {
+        playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+    }
+
+    void showFluffyUnicorns() {
+        playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+    }
+
+    void playVideo(String videoID) {
+
+
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+                if (Runtime.getRuntime().exec(new String[] {
+                        "which", "xdg- open" }).getInputStream().read() != -1) {
+                    Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+                }
+            } else {
+                URI uri = new URI(videoID);
+                java.awt.Desktop.getDesktop().browse(uri);
+            }      } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
